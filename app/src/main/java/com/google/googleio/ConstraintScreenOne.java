80013c8440177806381cda5aaed8446163cde0d8
Prof-Mohamed Atef <proofesser@gmail.com>
2020-05-16 06:53:47 +0200

    set a chain constraint

---
+++ app/src/main/java/com/google/googleio/ConstraintScreenOne.java
@@ -0,0 +1,13 @@
+package com.google.googleio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConstraintScreenOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_screen_one);
+    }
+}
